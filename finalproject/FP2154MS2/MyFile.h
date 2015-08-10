d791329aba60f29edbf07acb503418cdaf65fad5
Larissa Voigt <hi@larissavoigt.com>
2015-08-10 16:29:51 -0400

    Adding M2

---
+++ finalproject/FP2154MS2/MyFile.h
@@ -0,0 +1,19 @@
+#ifndef __244_ReadWrite_H__
+#define __244_ReadWrite_H__
+#include "ReadWrite.h"
+namespace oop244{
+  class MyFile : public ReadWrite {
+      char _fname[256];
+      char _text[10000];
+    public:
+      MyFile(const char* fname);
+      std::fstream& store(std::fstream& file)const;
+      std::fstream& load(std::fstream& file);
+      std::ostream& display(std::ostream& os, bool linear)const;
+      std::istream& conInput(std::istream& is);
+      void print();
+    };
+  std::ostream& operator<<(std::ostream& os, const MyFile& RO);
+  std::istream& operator>>(std::istream& id, MyFile& RO);
+}
+#endif
---
+++ finalproject/FP2154MS2/ReadWriteTester.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "MyFile.h"
+using namespace std;
+using namespace oop244;
+int main(){
+  fstream file;
+  MyFile F("readwrite.txt");
+  cout << "Run this program few times and keep adding to" <<endl<<"the content of the file created." << endl;
+  F.load(file);
+  cout << "Linear content of file readwrite.txt: " << endl << endl << F << endl;
+  cout << "Press <ENTER> to continue...";
+  cin.ignore();
+  cout << endl << "readwrite.txt as is: " << endl;
+  F.print();
+  cout << " ---------------- " << endl << "Enter a text and then"
+        <<endl<<"On windows end it by pressing <Ctrl-Z> and <ENTER> in a new line"
+        <<endl<<"On Linux or mac end it with a new line with <Ctrl-D>"
+        <<endl<< "to append to the end of readwrite.txt:" << endl;
+  cin >> F;
+  F.store(file);
+  cout << "Re-run the program to see the appended lines." << endl;
+  return 0;
+}
